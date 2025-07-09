33a2b82c07f9860a3b5eab7e5653b113b8262c44
tmdgo01 <seunhgye0629@gmail.com>
2025-07-09 17:54:12 +0900

    day3finalQ

--- day3/day3.dart
+++ day3/day3.dart
@@ -6,18 +6,36 @@ void main() {
   ];
   print(evenNumbers);
 
-  // TODO : Map을 이용하여 생선 가게에 다음과 같은 생선들이 존재하도록 코드 작성
-  // {"도미": 5, "참치": 3, "연어": 7, "광어": 13}
-  Map fishdb = {"도미": 5, "참치": 3, "연어": 7, "광어": 13};
-  print(fishdb);
-
-  // Player Class를 사용한 예제 print 코드 작성
+  // Player Class를 사용한 print
   Player player = Player("Joe", 25);
-  print("이름: ${player.name}, 나이: ${player.age}");
+  print("name: ${player.name}, age: ${player.age}");
 
-  player.name = "Rumi";
+  player.name = "Rumira";
   player.age = 22;
   print("setter name: ${player.name}, setter age: ${player.age}");
+
+  // TODO : Map을 이용하여 생선 가게에 다음과 같은 생선들이 존재하도록 코드 작성
+  // {"도미": 5, "참치": 3, "연어": 7, "광어": 13}
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // TODO : Player 클래스 - 생선을 구매할 수 있음
+  // {"도미": 1, "참치": 2, "연어": 3, "광어": 14}를 구매함
+  // 생선을 구매하는 함수(buy) 구현
+  // 생선을 구매할 수 있는 Player class, 구매할 경우 fishdb 내의 생선 수가 줄어 든다
+  // Class를 사용하여 사용자 가방 Map(backpack) 생성, 구매할 경우 생선 수가 늘어남
+  // 생선 가게의 생선 수가 0 이하일 경우, [구매할 수 있는 생선이 없습니다.] 출력
+  // 사용자의 가방과 생선 가게의 생선 수를 확인할 수 있는 기능 구현(함수)
+
+  // 생선 정보 저장
+  Map<String, int> fishdb = {"도미": 5, "참치": 3, "연어": 7, "광어": 13};
+  FishStore store = FishStore(fishdb); // FishStore에서 사용할 값(fishdb)
+  store.printFishStock(); // 생선 가게 재고 출력
+
+  // 구매 예시
+  Map<String, int> order = {"도미": 2, "참치": 3, "연어": 3, "광어": 14};
+  player.buy(order, store);
+
+  player.printBackpack();
+  store.printFishStock();
 }
 
 // TODO : name과 age를 입력받을 수 있는 Player Class를 만들고
@@ -41,4 +59,46 @@ class Player {
 
   // 나이 setter
   set age(int age) => _age = age;
+
+  Map<String, int> backpack = {};
+
+  // 플레이어 가방 내역 확인
+  void printBackpack() {
+    print("$name's backpack : $backpack");
+  }
+
+  // 생선 구매 함수
+  void buy(Map<String, int> order, FishStore store) {
+    for (var fish in order.keys) {
+      int want = order[fish] ?? 0;
+      int stock = store.fishdb[fish] ?? 0;
+      if (stock < want || stock <= 0) {
+        print("구매할 수 있는 생선이 없습니다. ($fish)");
+        continue;
+      }
+      // 가게 재고 차감
+      store.fishdb[fish] = stock - want;
+      // 가방에 추가
+      backpack[fish] = (backpack[fish] ?? 0) + want;
+      print("$name이(가) $fish $want개를 구매했습니다.");
+    }
+  }
+}
+
+class FishStore {
+  Map<String, int> fishdb;
+
+  FishStore(this.fishdb);
+
+  // 생선 가게의 생선 수 확인
+  void printFishStock() {
+    print("생선 가게 재고: $fishdb");
+    // if 생선 가게의 재고를 넘어가게 구매하면 최대 수량까지만 구매 가능
+    for (var entry in fishdb.entries) {
+      if (entry.value < 0) {
+        fishdb[entry.key] = 0;
+        print("[${entry.key}]를 더 이상 구매할 수 없습니다.");
+      }
+    }
+  }
 }
